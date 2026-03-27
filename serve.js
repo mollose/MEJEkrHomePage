e284a0f3910006e8ba8e7a9473d4d805fce2cd67
molloy <zweispringerspott@gmail.com>
2026-03-27 12:41:15 +0900

    2026/03/27

---
+++ serve.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const ROOT = __dirname;
+
+const MIME = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'application/javascript; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+  '.woff2': 'font/woff2',
+};
+
+http.createServer((req, res) => {
+  let url = req.url.split('?')[0];
+  if (url === '/') url = '/index.html';
+  const filePath = path.join(ROOT, decodeURIComponent(url));
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`Serving on http://localhost:${PORT}`);
+});
